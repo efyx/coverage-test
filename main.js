6063480995edbd2adb9af279a066057ec4a30ef4
efyx <efyx.ps@gmail.com>
2022-04-06 14:15:00 +0200

    Throw error if x or y isn't a number

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 module.exports = {
   add(x, y) {
+    if (isNaN(x)) {
+      throw new Error("x is not a number");
+    } else if (isNaN(y)) {
+      throw new Error("y is not a number");
+    }
     return x + y;
   }
 }
+
